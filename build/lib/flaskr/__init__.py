e3bc868f8a9614c1be6259a3c13e6b6dff6b5c18
Marco <marcohdes94i@gmail.com>
2018-06-05 18:46:06 -0600

    uno

---
+++ build/lib/flaskr/__init__.py
@@ -0,0 +1,44 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config = None):
+
+	#create and configure theapp
+
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY='dev',
+		DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+		)
+
+	if test_config is None:
+		#load the instance cofig, if it exist,when not testing
+		app.config.from_pyfile('config.py', silent = True)
+
+	else:
+
+		#load the test config if passed in
+		app.config.from_mapping(test_config)
+
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+
+
+	from . import db
+	db.init_app(app)
+
+	#return app	
+
+
+	from . import auth
+	app.register_blueprint(auth.bp)
+
+	from . import index
+	app.register_blueprint(index.bp)
+	app.add_url_rule('/', endpoint='index')
+
+	return app
